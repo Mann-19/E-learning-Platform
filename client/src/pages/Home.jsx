3c96f1ba5a65d775efafe58c0b5b6bb19b08e431
Mann-19 <tkp2510@gmail.com>
2025-04-15 21:22:56 +0530

    minor fix

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,11 +2,11 @@ import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
-    <div className=" text-3xl bg-black h-[100vh]">
+    <div className=" text-3xl h-[100vh]">
       <Navbar />
 
       <main className="px-20">
-        <p className="text-white text-base">Home page stuff</p>
+        <p className="text-black text-base">Home page stuff</p>
       </main>
     </div>
   )
